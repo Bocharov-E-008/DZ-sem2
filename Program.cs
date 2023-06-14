cda8adb20457e8e0b18db1af349c7b6fe0ca8349
Evgeny_B <jonn-x008@yandex.ru>
2023-06-14 03:48:32 +0300

    Задачи на 2 семинар

--- Program.cs
+++ Program.cs
@@ -17,19 +17,77 @@ else
 }
 
 
-
-
-
-
-
-
-
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+    int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+    return result;
+}
+
+
+
+
+
+
+
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(WorkHoliday(dayNumber));
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+string WorkHoliday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write("Цифра " + a + " - Выходной");
+        }
+        else
+        {
+            Console.Write("Цифра" + a + " - Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("День не определен");
+    }
+    return " день.";
+}
